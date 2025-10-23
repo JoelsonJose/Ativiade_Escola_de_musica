085ec35b5d86d379e6167aa2b5e7465ae71c4787
Joelson José <joelsonjose222@gmail.com>
2025-10-23 17:51:40 -0300

    Update SQL-DDL-DROP.sql

--- SQL-DDL-DROP.sql
+++ SQL-DDL-DROP.sql
@@ -2,9 +2,8 @@
 
 USE `Escola_de_Musica_do_Recife`;
 
--- -----------------------------------------------------
--- Etapa 1: Drop de todas as Views
--- -----------------------------------------------------
+-- Drop de todas as Views
+
 DROP VIEW IF EXISTS `v_mediaidadeorquestra`;
 DROP VIEW IF EXISTS `v_musicosapartir2024`;
 DROP VIEW IF EXISTS `v_musicoscordasorquestrasantes2000`;
@@ -16,9 +15,9 @@ DROP VIEW IF EXISTS `v_quantosmusicoscadaorquestra`;
 DROP VIEW IF EXISTS `v_relatoriocompleto`;
 DROP VIEW IF EXISTS `v_sinfoniasmaestroalgumaexecucao`;
 
--- -----------------------------------------------------
--- Etapa 2: Drop de todas as Tabelas
--- -----------------------------------------------------
+
+-- Drop de todas as Tabelas
+
 DROP TABLE IF EXISTS `MusicosFuncaoSinfonia`;
 DROP TABLE IF EXISTS `MusicosInstrumentos`;
 DROP TABLE IF EXISTS `HabilidadeMusical`; -- Adicionado para caso tenha renomeado
